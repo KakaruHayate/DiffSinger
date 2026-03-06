1b7dc13a6e4bee7c7c767f47eea7ba67f17fe964
KakaruHayate <kakaru.hk@hotmail.com>
2026-03-06 17:41:13 +0800

    Simplify LYNXNet2: remove bottleneck & GLU option
    Remove optional bottleneck and GLU-type selection from LYNXNet2 and configs. The configs no longer include use_bottleneck_layer or bottleneck_dim. LYNXNet2 now always uses a single nn.Linear input_projection, a Linear conditioner_projection (conditioner cache logic removed), and hardcodes SwiGLU in LYNXNet2Block (ATanGLU and glu_type removed). Constructor signature simplified (dropout param retained), residual layers updated to use the single dropout arg, and weight initializations adjusted. These changes simplify the model API and remove branching logic for bottleneck/activation and conditioner cache.

--- modules/backbones/lynxnet2.py
+++ modules/backbones/lynxnet2.py
@@ -2,20 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from modules.commons.common_layers import SinusoidalPosEmb, SwiGLU, ATanGLU, Transpose
+from modules.commons.common_layers import SinusoidalPosEmb, SwiGLU, Conv1d, Transpose
 from utils.hparams import hparams
 
 
 class LYNXNet2Block(nn.Module):
-    def __init__(self, dim, expansion_factor, kernel_size=31, dropout=0., glu_type='swiglu'):
+    def __init__(self, dim, expansion_factor, kernel_size=31, dropout=0.):
         super().__init__()
         inner_dim = int(dim * expansion_factor)
-        if glu_type == 'swiglu':
-            _glu = SwiGLU()
-        elif glu_type == 'atanglu':
-            _glu = ATanGLU()
-        else:
-            raise ValueError(f'{glu_type} is not a valid activation')
         if float(dropout) > 0.:
             _dropout = nn.Dropout(dropout)
         else:
@@ -26,9 +20,9 @@ class LYNXNet2Block(nn.Module):
             nn.Conv1d(dim, dim, kernel_size=kernel_size, padding=kernel_size // 2, groups=dim),
             Transpose((1, 2)),
             nn.Linear(dim, inner_dim * 2),
-            _glu,
+            SwiGLU(),
             nn.Linear(inner_dim, inner_dim * 2),
-            _glu,
+            SwiGLU(),
             nn.Linear(inner_dim, dim),
             _dropout
         )
@@ -39,30 +33,15 @@ class LYNXNet2Block(nn.Module):
 
 class LYNXNet2(nn.Module):
     def __init__(self, in_dims, n_feats, *, num_layers=6, num_channels=512, expansion_factor=1, kernel_size=31,
-                 dropout_rate=0.0, use_conditioner_cache=False, glu_type='swiglu', use_bottleneck_layer=False, bottleneck_dim=128):
+                 dropout=0.0):
         """
         LYNXNet2(Linear Gated Depthwise Separable Convolution Network Version 2)
         """
         super().__init__()
         self.in_dims = in_dims
         self.n_feats = n_feats
-        if use_bottleneck_layer:
-            self.input_projection = nn.Sequential(
-                nn.Linear(in_dims * n_feats, bottleneck_dim, bias=False),
-                nn.Linear(bottleneck_dim, num_channels)
-            )
-            nn.init.kaiming_normal_(self.input_projection[0].weight)
-            nn.init.kaiming_normal_(self.input_projection[1].weight)
-            nn.init.constant_(self.input_projection[1].bias, 0)
-        else:
-            self.input_projection = nn.Linear(in_dims * n_feats, num_channels)
-            nn.init.kaiming_normal_(self.input_projection.weight)
-        self.use_conditioner_cache = use_conditioner_cache
-        if self.use_conditioner_cache:
-            # It may need to be modified at some point to be compatible with the condition cache
-            self.conditioner_projection = nn.Conv1d(hparams['hidden_size'], num_channels, 1)
-        else:
-            self.conditioner_projection = nn.Linear(hparams['hidden_size'], num_channels)
+        self.input_projection = nn.Linear(in_dims * n_feats, num_channels)
+        self.conditioner_projection = nn.Linear(hparams['hidden_size'], num_channels)
         self.diffusion_embedding = nn.Sequential(
             SinusoidalPosEmb(num_channels),
             nn.Linear(num_channels, num_channels * 4),
@@ -75,15 +54,14 @@ class LYNXNet2(nn.Module):
                     dim=num_channels,
                     expansion_factor=expansion_factor,
                     kernel_size=kernel_size,
-                    dropout=dropout_rate,
-                    glu_type=glu_type
+                    dropout=dropout
                 )
                 for i in range(num_layers)
             ]
         )
         self.norm = nn.LayerNorm(num_channels)
         self.output_projection = nn.Linear(num_channels, in_dims * n_feats)
-        
+        nn.init.kaiming_normal_(self.input_projection.weight)
         nn.init.kaiming_normal_(self.conditioner_projection.weight)
         nn.init.zeros_(self.output_projection.weight)
 
@@ -101,11 +79,7 @@ class LYNXNet2(nn.Module):
             x = spec.flatten(start_dim=1, end_dim=2)  # [B, F x M, T]
 
         x = self.input_projection(x.transpose(1, 2)) # [B, T, F x M]
-        if self.use_conditioner_cache:
-            # It may need to be modified at some point to be compatible with the condition cache
-            x = x + self.conditioner_projection(cond).transpose(1, 2)
-        else:
-            x = x + self.conditioner_projection(cond.transpose(1, 2))
+        x = x + self.conditioner_projection(cond.transpose(1, 2))
         x = x + self.diffusion_embedding(diffusion_step).unsqueeze(1)
 
         for layer in self.residual_layers:
