fix wrong variance name

modules/fastspeech/acoustic_encoder.py
@@ -112,6 +112,8 @@ class FastSpeech2Acoustic(nn.Module):
                 self.variance_embeds[v_name](variances[v_name][:, :, None] * self.variance_scaling_factor[v_name])
                 for v_name in self.variance_embed_list
             ], dim=-1).sum(-1)
+            # print(variances['tension'].shape)
+            # print(variances['mouth_opening'].shape)
             condition += variance_embeds
 
         if self.use_key_shift_embed:
@@ -188,7 +190,7 @@ class FastSpeech2Acoustic(nn.Module):
                 speed_embed = self.speed_embed(speed[:, :, None] * self.variance_scaling_factor['speed'])
                 condition_shm = condition_shm + speed_embed.detach()
 
-            opec = kwargs.get('opec')
+            opec = kwargs['mouth_opening']
             if opec is not None:
                 opec_emb = self.opec_embed(opec[:, :, None])
                 condition_shm = condition_shm + opec_emb

modules/toplevel.py
@@ -164,7 +164,7 @@ class DiffSingerAcoustic(CategorizedModule, ParameterAdaptorModule):
                 if torch.rand(1).item() < p_replace:
                     alpha = (torch.rand((condition.shape[0], condition.shape[1], 1), device=condition.device) * 2 - 1)
                     
-                    gt_opec = kwargs.get('opec')
+                    gt_opec = kwargs.get('mouth_opening')
                     shifted_opec = calculate_shifted_opec(gt_opec, -alpha.squeeze(-1), o_min=self.opec_min, o_max=self.opec_max)
                     
                     gt_opec_emb = self.fs2.opec_embed(gt_opec[:, :, None])