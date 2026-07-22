e1ffd50956ac3e0ee8925c1291c6042501691a89
KakaruHayate <kakaru.hk@hotmail.com>
2026-07-22 21:56:18 +0800

    low rope theta / laurel block

--- modules/fastspeech/tts_modules.py
+++ modules/fastspeech/tts_modules.py
@@ -373,7 +373,8 @@ class FastSpeech2Encoder(nn.Module):
             self, hidden_size, num_layers,
             ffn_kernel_size=9, ffn_act='gelu',
             dropout=None, num_heads=2, use_pos_embed=True, rel_pos=True,
-            use_rope=False, rope_interleaved=True, mix_ln_layer=None
+            use_rope=False, rope_interleaved=True, mix_ln_layer=None, rope_theta=10000,
+            use_laurel_block=False
     ):
         super().__init__()
         self.num_layers = num_layers
@@ -394,7 +395,7 @@ class FastSpeech2Encoder(nn.Module):
                 self.hidden_size, self.dropout,
                 kernel_size=ffn_kernel_size, act=ffn_act,
                 num_heads=num_heads, rotary_embed=rotary_embed,
-                layer_idx=i, mix_ln_layer=mix_ln_layer
+                layer_idx=i, mix_ln_layer=mix_ln_layer, use_laurel_block=use_laurel_block
             )
             for i in range(self.num_layers)
         ])
--- modules/fastspeech/variance_encoder.py
+++ modules/fastspeech/variance_encoder.py
@@ -34,7 +34,8 @@ class FastSpeech2Variance(nn.Module):
             ffn_kernel_size=hparams['enc_ffn_kernel_size'], ffn_act=hparams['ffn_act'],
             dropout=hparams['dropout'], num_heads=hparams['num_heads'],
             use_pos_embed=hparams['use_pos_embed'], rel_pos=hparams.get('rel_pos', False), 
-            use_rope=hparams.get('use_rope', False), rope_interleaved=hparams.get('rope_interleaved', True)
+            use_rope=hparams.get('use_rope', False), rope_interleaved=hparams.get('rope_interleaved', True), 
+            rope_theta=hparams.get('rope_theta', 10000),use_laurel_block=hparams.get('use_laurel_block', False),
         )
 
         dur_hparams = hparams['dur_prediction_args']
