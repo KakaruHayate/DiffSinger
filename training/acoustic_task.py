141a187fffca9251a7c0bfbdd918979c7131189d
KakaruHayate <kakaru.hk@hotmail.com>
2026-07-31 17:08:25 +0800

    fix: integrate reviewed mel padding correction (#313)
    Source: openvpi/DiffSinger PR #313 at 5f2a10fa96d3706006f73f3be458b52fc3fce9af.

--- training/acoustic_task.py
+++ training/acoustic_task.py
@@ -45,7 +45,8 @@ class AcousticDataset(BaseDataset):
         tokens = utils.collate_nd([s['tokens'] for s in samples], 0)
         f0 = utils.collate_nd([s['f0'] for s in samples], 0.0)
         mel2ph = utils.collate_nd([s['mel2ph'] for s in samples], 0)
-        mel = utils.collate_nd([s['mel'] for s in samples], 0.0)
+        mel_pad = float(hparams['spec_min'][0]) if hparams.get('spec_min') else -12.0
+        mel = utils.collate_nd([s['mel'] for s in samples], mel_pad)
         batch.update({
             'tokens': tokens,
             'mel2ph': mel2ph,
